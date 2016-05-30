bd7f45b2ed432752e4e01bdf20320f09865e37cc
DineshAjay <p.dinesh88@gmail.com>
2016-05-30 08:59:29 +0530

    commited

---
+++ app/src/main/java/com/dpk/dinesh/mapservice/InfoWindoAdapter.java
@@ -0,0 +1,7 @@
+package com.dpk.dinesh.mapservice;
+
+/**
+ * Created by DINESH on 21-May-16.
+ */
+public class InfoWindoAdapter {
+}
---
+++ app/src/main/java/com/dpk/dinesh/mapservice/LocationInput.java
@@ -0,0 +1,7 @@
+package com.dpk.dinesh.mapservice;
+
+/**
+ * Created by DINESH on 29-May-16.
+ */
+public class LocationInput {
+}
